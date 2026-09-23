db99e4b70169158a1cf887d12a0f84ca6a61c554
Thalyta Lima <thalytalima211@gmail.com>
2026-09-23 15:14:57 -0300

    feat: implements first components

--- study-flow/src/views/HomeView.vue
+++ study-flow/src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <header class="navbar navbar-dark bg-dark px-3">
+    <span class="navbar-brand mb-0 h1">StudyFlow</span>
+  </header>
+
+  <main class="container my-4">
+
+    <section aria-label="Disciplinas e Tarefas" class="row mt-4">
+      <CourseList />
+    </section>
   </main>
 </template>
+
+<script setup>
+import CourseList from '@/components/CourseList.vue'
+</script>
